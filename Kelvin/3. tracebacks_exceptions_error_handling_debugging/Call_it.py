58d605e9bbc5e887fc17893c163cc9945efe964d
kelvinmacharia254 <pilotkelvin0@gmail.com>
2023-12-31 11:20:42 +0300

    Add tracebacks notebook

---
+++ Kelvin/3. tracebacks_exceptions_error_handling_debugging/Call_it.py
@@ -0,0 +1,3 @@
+import sample_failing_code
+
+print(sample_failing_code.addTwoNumbers2(1, 2))
---
+++ Kelvin/3. tracebacks_exceptions_error_handling_debugging/sample_failing_code.py
@@ -0,0 +1,17 @@
+# def addTwoNumbers(x, y):
+#
+#     return x + y
+
+
+# print(addTwoNumbers(1, q))
+
+
+def addTwoNumbers2(x, y):
+    z = x + q
+    return z
+
+# self test code
+if __name__ == "__main__":
+    print(addTwoNumbers2(1, 2))
+
+# %%
